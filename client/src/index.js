80eb63324e95f92c16ffbee1ad7c75e5a4214a11
William-Cruz <chiefxcruz@gmail.com>
2018-11-16 19:11:56 -0600

    added webconfig

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import AppContainer from "./AppContainer";
 import "./index.css";
 import store from "./store";
 import {Provider} from "react-redux";
+import "./web.config";
 
 ReactDOM.render(
   <Provider store={store}><AppContainer /></Provider>,
